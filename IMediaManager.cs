9c6676200faa84af8466e3bf698016186b00363d
Artursdur <Manykian8889@gmail.com>
2025-03-20 17:02:33 +0500

    Добавьте файлы проекта.

---
+++ IMediaManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    using System.Collections.Generic;
+
+    public interface IMediaManager<T>
+    {
+        void Add(T item); 
+        bool Remove(string title); 
+        T FindByTitle(string title);
+        List<T> GetAllAvailable();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var bookLibrary = new Library<Book>();
+
+            try
+            {
+                bookLibrary.Add(new Book("1984", "George Orwell", 1949, 328, "Dystopian"));
+                bookLibrary.Add(new Book("To Kill a Mockingbird", "Harper Lee", 1960, 281, "Fiction"));
+
+                Console.WriteLine("Все книги в библиотеке:");
+                bookLibrary.PrintAll();
+
+                bookLibrary.CheckOut("1984");
+
+                Console.WriteLine("\nДоступные книги:");
+                var availableBooks = bookLibrary.GetAllAvailable();
+                foreach (var book in availableBooks)
+                {
+                    Console.WriteLine($"Название: {book.Title}, Автор: {book.Author}");
+                }
+
+                bookLibrary.Return("1984");
+
+                bookLibrary.Remove("To Kill a Mockingbird");
+                Console.WriteLine("\nПосле удаления книги 'To Kill a Mockingbird':");
+                bookLibrary.PrintAll();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+    }
+}
